de6f9e6ad1a208b345d421b867c75da0ff8d9b1a
Roman Royik <r.royik@gmail.com>
2014-08-04 11:36:21 +0300

    Project init

---
+++ components/MysqlColumnSchema.php
@@ -0,0 +1,31 @@
+<?php
+namespace aggregable\yiidbtypecast\components;
+
+use \CMysqlColumnSchema;
+
+/**
+ * Class MysqlColumnSchema
+ */
+class MysqlColumnSchema extends CMysqlColumnSchema {
+	/**
+	 * @inheritdoc
+	 */
+	protected function extractType($dbType) {
+		if (preg_match('/(int|bit|tinyint|smallint|mediumint)/', $dbType)) {
+			$this->type = 'integer';
+		} else {
+			parent::extractType($dbType);
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function typecast($value) {
+		if ($this->type === 'double') {
+			return (float)$value;
+		} else {
+			return parent::typecast($value);
+		}
+	}
+}
---
+++ components/MysqlScheme.php
@@ -0,0 +1,35 @@
+<?php
+namespace aggregable\yiidbtypecast\components;
+
+use \CMysqlSchema;
+
+/**
+ * Class MysqlScheme
+ */
+class MysqlScheme extends CMySqlSchema {
+	/**
+	 * @inheritdoc
+	 */
+	protected function createColumn($column) {
+		$c = $this->createColumnScheme();
+		$c->name = $column['Field'];
+		$c->rawName = $this->quoteColumnName($c->name);
+		$c->allowNull = $column['Null'] === 'YES';
+		$c->isPrimaryKey = strpos($column['Key'], 'PRI') !== false;
+		$c->isForeignKey = false;
+		$c->init($column['Type'], $column['Default']);
+		$c->autoIncrement = strpos(strtolower($column['Extra']), 'auto_increment') !== false;
+		if (isset($column['Comment'])) {
+			$c->comment = $column['Comment'];
+		}
+
+		return $c;
+	}
+
+	/**
+	 * @return MysqlColumnSchema
+	 */
+	protected function createColumnScheme() {
+		return new MysqlColumnSchema();
+	}
+}
