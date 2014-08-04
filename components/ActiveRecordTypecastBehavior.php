aa4a86e85f3b7e847b7df3cfdff77a1dd3f41d6d
Roman Royik <r.royik@gmail.com>
2014-08-04 12:50:02 +0300

    Added ActiveRecord Typecast Behavior

---
+++ components/ActiveRecordTypecastBehavior.php
@@ -0,0 +1,24 @@
+<?php
+namespace aggregable\yiidbtypecast\components;
+
+use \CActiveRecordBehavior,
+	\CDbColumnSchema,
+	\CActiveRecord;
+
+/**
+ * Class ActiveRecordTypecastBehavior
+ *
+ * @method \CActiveRecord getOwner()
+ */
+class ActiveRecordTypecastBehavior extends CActiveRecordBehavior {
+	public function afterFind($event) {
+		$md = $this->getOwner()->getMetaData();
+		foreach ($this->getOwner()->getAttributes() as $name => $attribute) {
+			if (isset($md->columns[$name])) {
+				/** @var CDbColumnSchema $c */
+				$c = $md->columns[$name];
+				$this->getOwner()->setAttribute($name, $c->typecast($attribute));
+			}
+		}
+	}
+}
