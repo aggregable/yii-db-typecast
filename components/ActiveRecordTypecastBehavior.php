3fabb18648d861daff53d23f34119a4fef769ee3
Roman Royik <r.royik@gmail.com>
2014-10-18 23:16:24 +0300

    PSR-2 code reformat

--- components/ActiveRecordTypecastBehavior.php
+++ components/ActiveRecordTypecastBehavior.php
@@ -2,26 +2,28 @@
 namespace aggregable\yiidbtypecast\components;
 
 use \CActiveRecordBehavior,
-	\CDbColumnSchema,
-	\CActiveRecord;
+    \CDbColumnSchema,
+    \CActiveRecord;
 
 /**
  * Class ActiveRecordTypecastBehavior
  *
  * @method \CActiveRecord getOwner()
  */
-class ActiveRecordTypecastBehavior extends CActiveRecordBehavior {
-	/**
-	 * @inheritdoc
-	 */
-	public function afterFind($event) {
-		$md = $this->getOwner()->getMetaData();
-		foreach ($this->getOwner()->getAttributes() as $name => $attribute) {
-			if (isset($md->columns[$name])) {
-				/** @var CDbColumnSchema $c */
-				$c = $md->columns[$name];
-				$this->getOwner()->setAttribute($name, $c->typecast($attribute));
-			}
-		}
-	}
+class ActiveRecordTypecastBehavior extends CActiveRecordBehavior
+{
+    /**
+     * @inheritdoc
+     */
+    public function afterFind($event)
+    {
+        $md = $this->getOwner()->getMetaData();
+        foreach ($this->getOwner()->getAttributes() as $name => $attribute) {
+            if (isset($md->columns[$name])) {
+                /** @var CDbColumnSchema $c */
+                $c = $md->columns[$name];
+                $this->getOwner()->setAttribute($name, $c->typecast($attribute));
+            }
+        }
+    }
 }
--- components/MysqlColumnSchema.php
+++ components/MysqlColumnSchema.php
@@ -6,28 +6,31 @@ use \CMysqlColumnSchema;
 /**
  * Class MysqlColumnSchema
  */
-class MysqlColumnSchema extends CMysqlColumnSchema {
-	/**
-	 * @inheritdoc
-	 */
-	protected function extractType($dbType) {
-		if (preg_match('/^(int|bit|tinyint|smallint|mediumint)/', $dbType)) {
-			$this->type = 'integer';
-		} elseif (strpos($dbType, 'decimal') !== false) {
-			$this->type='double';
-		} else {
-			parent::extractType($dbType);
-		}
-	}
+class MysqlColumnSchema extends CMysqlColumnSchema
+{
+    /**
+     * @inheritdoc
+     */
+    protected function extractType($dbType)
+    {
+        if (preg_match('/^(int|bit|tinyint|smallint|mediumint)/', $dbType)) {
+            $this->type = 'integer';
+        } elseif (strpos($dbType, 'decimal') !== false) {
+            $this->type = 'double';
+        } else {
+            parent::extractType($dbType);
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function typecast($value) {
-		if ($this->type === 'double') {
-			return (float)$value;
-		} else {
-			return parent::typecast($value);
-		}
-	}
+    /**
+     * @inheritdoc
+     */
+    public function typecast($value)
+    {
+        if ($this->type === 'double') {
+            return (float)$value;
+        } else {
+            return parent::typecast($value);
+        }
+    }
 }
